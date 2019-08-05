eba1870a27d6bf738dcf6e9cd55c77f673ec55fc
Erdem Uslu <usluerdem@yandex.com>
2019-08-05 11:11:00 +0300

    test useEffect re-render conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => (
-  <div>
-    React app rendered
-  </div>
-)
+import Child from './Child'
+
+const App = () => {
+  const [item, setItem] = useState('Erdem')
+  const [value, setValue] = useState('')
+
+  const updateItem = () => {
+    setItem(value)
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={e => setValue(e.target.value)}
+      />
+      <button type="button" onClick={updateItem}>
+        Update Item
+      </button>
+      <Child
+        name={item}
+      />
+    </div>
+  )
+}
 
 export default App
