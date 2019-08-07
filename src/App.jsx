24c891c45c9e28e1d1adbcad2fbb39b97c6a75bb
Erdem Uslu <usluerdem@yandex.com>
2019-08-07 10:46:31 +0300

    Revert "test useEffect re-render conditions"
    This reverts commit eba1870a27d6bf738dcf6e9cd55c77f673ec55fc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import Child from './Child'
-
-const App = () => {
-  const [item, setItem] = useState('Erdem')
-  const [value, setValue] = useState('')
-
-  const updateItem = () => {
-    setItem(value)
-  }
-
-  return (
-    <div>
-      <input
-        type="text"
-        onChange={e => setValue(e.target.value)}
-      />
-      <button type="button" onClick={updateItem}>
-        Update Item
-      </button>
-      <Child
-        name={item}
-      />
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    React app rendered
+  </div>
+)
 
 export default App
