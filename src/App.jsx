d1fe7dfc51b464c5b9a8cf71a2a090387228538d
Erdem Uslu <usluerdem@yandex.com>
2019-11-04 17:58:55 +0300

    Upgrade react version

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const App = () => (
-  <div>
-    React app rendered
-  </div>
-);
+const App = () => {
+  useEffect(() => {
+    async function fetchImg() {
+      try {
+        const res = await fetch('https://cataas.com/cat/says/hello');
+        const data = res.json();
+
+        console.log(data);
+      } catch (err) {
+        console.loog(err);
+      }
+    }
+
+    fetchImg();
+  }, []);
+
+  return (
+    <div>
+      React app rendered
+    </div>
+  );
+};
 
 export default App;
